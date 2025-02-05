330cec20e2f8d0b8a7b5a44ab94ecbdcc3e86727
TaiFong <codefong.c@gmail.com>
2025-02-04 19:05:39 -0800

    Creating Window

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-clang src/main.cpp -o main2d.exe
+libs=-luser32
+warnings="-Wno-writable-strings -Wno-format-security"
+clang++ -g src/main.cpp -omain2d.exe $libs $warnings
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,24 @@
 
+
+// ################
+// Platform Globals 
+// ################
 static bool running = true;
 
+// ##################
+// Platform Functions 
+// ##################
+bool platform_create_window(int width, int height, char* title);
+
+
+// ################
+// Windows Platform 
+// ################
+
 int main(){
+
+platform_create_window(1200, 720, "Fong");
+
     while(running){
         // Update
     }
